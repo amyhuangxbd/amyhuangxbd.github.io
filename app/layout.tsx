47f13fd0e67ea9eba1b99b451b8920d8b2af9fda
amyhuangxbd <huangxbd1990@126.com>
2024-06-13 15:51:07 +0800

    add google ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from 'next/script';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,6 +18,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3738291256196081"
+     crossOrigin="anonymous"></Script>
     </html>
   );
 }
