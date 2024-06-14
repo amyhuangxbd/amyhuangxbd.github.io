29f8d28bfdfc481bc30ecdefed4af194206beb5b
amyhuangxbd <huangxbd1990@126.com>
2024-06-14 14:09:13 +0800

    add some link

--- app/practice/page.tsx
+++ app/practice/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 const manifest = [{
     title: 'scroll',
@@ -7,7 +8,23 @@ const manifest = [{
 const Page = () => {
     return (
         <div>
-            
+            <section>
+                <h1>SVG</h1>
+                
+                <Link href={`/nextjs-blog-deployment/svg`} >Basic Graph</Link>
+                <Link href={`/nextjs-blog-deployment/svg/bauble`} >Bauble</Link>
+            </section>
+            <section>
+                <h1>threejs</h1>
+                <Link href={`/nextjs-blog-deployment/animate`} >人物</Link>
+                <Link href={`/nextjs-blog-deployment/ankou`} >threejs move</Link>
+                <Link href={`/nextjs-blog-deployment/cybertruck`} >threejs cybertruck</Link>
+                <Link href={`/nextjs-blog-deployment/level-one`} >level one</Link>
+                <Link href={`/nextjs-blog-deployment/physics`} >physics</Link>
+                <Link href={`/nextjs-blog-deployment/scrolling`} >scrolling</Link>
+                <Link href={`/nextjs-blog-deployment/terials`} >terials</Link>
+
+            </section>
         </div>
     );
 };
